59343b1b61eddd7a2f2363aa11d22166bfed2417
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2016-09-04 21:10:31 +0200

    040916

---
+++ 42/GetNextLine/getnextline1_nogit/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fklein <fklein@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/04 18:12:43 by fklein            #+#    #+#             */
+/*   Updated: 2016/09/04 18:25:53 by fklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 8
+
+char	ft_strjoin(char const *s1, char const *s2);
+void	ft_strclr(char *s);
+
+#endif
