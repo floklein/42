44680e0c08b8e15a7f7f51fd224e21a159f3336c
Florent Klein <flo-klein@hotmail.fr>
2016-11-14 21:02:52 +0100

    Add files via upload

--- 42/PrintF/printftest/main.c
+++ 42/PrintF/printftest/main.c
@@ -6,7 +6,7 @@
 /*   By: fklein <fklein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 19:14:57 by fklein            #+#    #+#             */
-/*   Updated: 2016/11/14 19:29:26 by nthibaud         ###   ########.fr       */
+/*   Updated: 2016/11/14 21:01:56 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,32 @@
 
 int		main(void)
 {
-	printf("");
-	ft_printf("");
+	printf("s = %s\n", s);
+	ft_printf("s = %s\n", s);
+//	printf("S = %s\n", S);
+//	ft_printf("S = %S\n", S);
+	printf("p = %p\n", p);
+	ft_printf("p = %p\n", p);
+	printf("d = %d\n", d);
+	ft_printf("d = %d\n", d);
+//	printf("D = %D\n", D);
+//	ft_printf("D = %D\n", D);
+//	printf("i = %i\n", i);
+//	ft_printf("i = %i\n", i);
+	printf("o = %o\n", o);
+	ft_printf("o = %o\n", o);
+//	printf("O = %O\n", O);
+//	ft_printf("O = %O\n", O);
+	printf("u = %u\n", u);
+	ft_printf("u = %u\n", u);
+//	printf("U = %U\n", U);
+//	ft_printf("U = %U\n", U);
+	printf("x = %x\n", x);
+	ft_printf("x = %x\n", x);
+	printf("X = %X\n", X);
+	ft_printf("X = %X\n", X);
+	printf("c = %c\n", c);
+	ft_printf("c = %c\n", c);
+//	printf("C = %C\n", C);
+//	ft_printf("C = %C\n", C);
 }
