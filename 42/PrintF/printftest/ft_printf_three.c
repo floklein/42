560773ec41b8c6a9ed8ee3cd62498cd38d264ddc
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2016-11-17 18:12:16 +0100

    PrintfOLD

---
+++ 42/PrintF/printftest/ft_printf_three.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_three.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fklein <fklein@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/17 18:31:23 by fklein            #+#    #+#             */
+/*   Updated: 2016/10/17 18:31:26 by fklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		ft_putnbrui(unsigned int i)
+{
+	int		nb;
+	long	m;
+
+	nb = 0;
+	m = 1;
+	if (i == 0)
+		return (ft_putchari('0'));
+	while (i != 0)
+	{
+		m *= 10;
+		m += i % 10;
+		i /= 10;
+	}
+	while (m != 1)
+	{
+		nb += ft_putchari((m % 10) + 48);
+		m /= 10;
+	}
+	return (nb);
+}
+
+static int		ft_putnbroi(unsigned int i)
+{
+	int		nb;
+	long	m;
+
+	nb = 0;
+	m = 1;
+	if (i == 0)
+		return (ft_putchari('0'));
+	while (i != 0)
+	{
+		m *= 8;
+		m += i % 8;
+		i /= 8;
+	}
+	while (m != 1)
+	{
+		nb += ft_putchari((m % 8) + 48);
+		m /= 8;
+	}
+	return (nb);
+}
+
+static int		ft_putnbrxi(unsigned int i)
+{
+	int		nb;
+	long	m;
+
+	nb = 0;
+	m = 1;
+	if (i == 0)
+		return (ft_putchari('0'));
+	while (i != 0)
+	{
+		m *= 16;
+		m += i % 16;
+		i /= 16;
+	}
+	while (m != 1)
+	{
+		if (m % 16 >= 10)
+			nb += ft_putchari((m % 16) + 87);
+		else
+			nb += ft_putchari((m % 16) + 48);
+		m /= 16;
+	}
+	return (nb);
+}
+
+static int		ft_putnbrmxi(unsigned int i)
+{
+	int		nb;
+	long	m;
+
+	nb = 0;
+	m = 1;
+	if (i == 0)
+		return (ft_putchari('0'));
+	while (i != 0)
+	{
+		m *= 16;
+		m += i % 16;
+		i /= 16;
+	}
+	while (m != 1)
+	{
+		if (m % 16 >= 10)
+			nb += ft_putchari((m % 16) + 55);
+		else
+			nb += ft_putchari((m % 16) + 48);
+		m /= 16;
+	}
+	return (nb);
+}
+
+static int		ft_putpti(long unsigned int i)
+{
+	int				nb;
+	unsigned long	m;
+
+	nb = 0;
+	m = 1;
+	nb += ft_putstri("0x");
+	if (i == 0)
+		return (nb + ft_putchari('0'));
+	while (i != 0)
+	{
+		m *= 16;
+		m += i % 16;
+		i /= 16;
+	}
+	while (m > 1)
+	{
+		if (m % 16 >= 10)
+			nb += ft_putchari((m % 16) + 87);
+		else
+			nb += ft_putchari((m % 16) + 48);
+		m /= 16;
+	}
+	return (nb);
+}
