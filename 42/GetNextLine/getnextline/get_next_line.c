ba2e3afe513b2cb39ab9f4147f5f7702370fa7b4
floklein <4128942+floklein@users.noreply.github.com>
2017-03-02 10:58:52 +0100

    02032017

---
+++ 42/GetNextLine/getnextline/get_next_line.c
@@ -0,0 +1,25 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+int	get_next_line(int fd, char **line)
+{
+	
+}
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("test", O_RDONLY, S_IREAD);
+	get_next_line(fd, line);
+}
