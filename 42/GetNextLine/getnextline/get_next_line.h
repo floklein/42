ab3d0d4883236f0499a492f2764573ade234b079
floklein <flo-klein@hotmail.fr>
2017-03-02 10:58:52 +0100

    02032017

---
+++ 42/GetNextLine/getnextline/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fklein <fklein@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/04 18:12:43 by fklein            #+#    #+#             */
+/*   Updated: 2016/09/05 18:36:50 by fklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 8
+
+int	get_next_line(const int fd, char **line);
+
+char	ft_strjoin(char const *s1, char const *s2);
+void	ft_strclr(char *s);
+int		ft_strlen(char *s);
+
+#endif
