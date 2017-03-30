bbf2449bbdb4a049a3d0a214cbf7c9dca7bbaaf5
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2017-03-30 09:20:59 +0200

    struct

---
+++ 42/PrintF/printf2/ft_printf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fklein <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/29 13:35:11 by fklein            #+#    #+#             */
+/*   Updated: 2017/03/29 13:57:48 by fklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+typedef struct	s_conv
+{
+	char		spec;
+	void		*data;
+	int			opt_hash;
+	int			opt_zero;
+	int			opt_minus;
+	int			opt_plus;
+	int			opt_space;
+	int			width_min;
+	int			precision;
+	int			mod_hh;
+	int			mod_h;
+	int			mod_l;
+	int			mod_ll;
+	int			mod_j;
+	int			mod_z;
+}				t_conv;
+
+#endif
