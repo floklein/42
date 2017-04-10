fdefcc20e09f374c1a75809a12533d44f753d687
floklein <4128942+floklein@users.noreply.github.com>
2017-04-10 20:24:20 +0200

    check done

--- 42/FdF/fdf1/main.c
+++ 42/FdF/fdf1/main.c
@@ -6,7 +6,7 @@
 /*   By: fklein <fklein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 15:46:10 by fklein            #+#    #+#             */
-/*   Updated: 2017/04/06 18:58:44 by fklein           ###   ########.fr       */
+/*   Updated: 2017/04/07 15:36:27 by fklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 #include <stdio.h>
 
-int	str_to_map(int ***map, char ***map_str, int map_width, int map_height)
+int	str_to_map(char ***map_str, t_data *data)
 {
 	int	i;
 	int	j;
 
-	if (!((*map) = (int **)malloc(sizeof(int *) * map_height)))
+	if (!((data->tab) = (int **)malloc(sizeof(int *) * data->height)))
 		return (0);
 	i = 0;
-	while (i < map_height)
+	while (i < data->height)
 	{
 		j = 0;
-		if (!((*map)[i] = (int *)malloc(sizeof(int) * map_width)))
+		if (!(data->tab[i] = (int *)malloc(sizeof(int) * data->width)))
 			return (0);
-		while (j < map_width)
+		while (j < data->width)
 		{
-			(*map)[i][j] = ft_atoi(map_str[i][j]);
+			data->tab[i][j] = ft_atoi(map_str[i][j]);
 			j++;
 		}
 		i++;
@@ -37,57 +37,59 @@ int	str_to_map(int ***map, char ***map_str, int map_width, int map_height)
 	return (1);
 }
 
-int	file_to_str_to_map(int ***map, char *file, int map_width, int map_height)
+int	file_to_str_to_map(char *file, t_data *data)
 {
 	char	***map_str;
 	int	i;
 	int	fd;
 	char	*line;
 
-	if (!(map_str = (char ***)malloc(sizeof(char **) * map_height)))
+	if (!(map_str = (char ***)malloc(sizeof(char **) * data->height)))
 		return (0);
 	i = 0;
 	if ((fd = open(file, O_RDONLY)) == -1)
 		return (no_file(file));
-	while (i < map_height)
+	while (i < data->height)
 	{
-		if(!(map_str[i] = (char **)malloc(sizeof(char*) * map_width)))
+		if(!(map_str[i] = (char **)malloc(sizeof(char*) * data->width)))
 			return (0);
 		get_next_line(fd, &line);
 		map_str[i] = ft_strsplit(line, ' ');
 		i++;
 	}
 	close(fd);
-	str_to_map(map, map_str, map_width, map_height);
+	str_to_map(map_str, data);
 	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	int	**map;
-	int	map_width = -1;
-	int	map_height = 0;
+	t_data	*data;
 	int	fd;
 	int	i;
 	int	j;
 
 	if (argc != 2)
-		ft_putstr_fd("Usage: ./fdf <filename> [ case_size z_size ]\n", 2);
-	else if (!check_file(&fd, argv[1], &map_width, &map_height))
+		return (errors(3));
+	else if (!(data = (t_data *)malloc(sizeof(t_data))))
 		return (0);
-	else if (!file_to_str_to_map(&map, argv[1], map_width, map_height))
-		ft_putstr_fd("map error\n", 2);
+	data->width = -1;
+	data->height = 0;
+	if (!check_file(&fd, argv[1], data))
+		return (0);
+	else if (!file_to_str_to_map(argv[1], data))
+		return (0);
 	else
 	{
 		printf("PRINTED:\n");
 		i = 0;
-		while (i < map_height)
+		while (i < data->height)
 		{
 			j = 0;
-			while (j < map_width)
+			while (j < data->width)
 			{
-				printf("%d ", map[i][j]);
-				if (map[i][j] < 10)
+				printf("%d ", data->tab[i][j]);
+				if (data->tab[i][j] < 10)
 					printf(" ");
 				j++;
 			}
