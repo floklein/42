d8506044ea54513c0415ebd9133934da990b1856
Florent KLEIN <flklein@e3r2p12.42.fr>
2018-08-30 15:05:34 +0200

    max pos

--- PiscineC2018/sastantua/ex00/sastantua.c
+++ PiscineC2018/sastantua/ex00/sastantua.c
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 11:49:59 by flklein           #+#    #+#             */
-/*   Updated: 2018/08/30 12:29:16 by flklein          ###   ########.fr       */
+/*   Updated: 2018/08/30 15:04:43 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,27 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	display_line(int floor, int line)
+void	display_line(int floor, int line, int *max_pos)
 {
 	int		pos;
 
 	pos = 1;
-	while (pos <= floor * (floor + 1) + line * 2)
+	while (pos <= *max_pos)
 	{
 		ft_putchar('o');
 		pos++;
 	}
+	*max_pos += 2;
 }
 
-void	display_floor(int floor)
+void	display_floor(int floor, int *max_pos)
 {
 	int		line;
 
 	line = 1;
 	while (line <= floor + 2)
 	{
-		display_line(floor, line);
+		display_line(floor, line, max_pos);
 		ft_putchar('\n');
 		line++;
 	}
@@ -43,11 +44,16 @@ void	display_floor(int floor)
 void	sastantua(int size)
 {
 	int		floor;
+	int		max_pos;
+	int		*p_max_pos;
 
 	floor = 1;
+	max_pos = 3;
+	p_max_pos = &max_pos;
 	while (floor <= size)
 	{
-		display_floor(floor);
+		display_floor(floor, p_max_pos);
+		max_pos += (floor % 2 ? (floor - 1) * 2 : (floor - 2) * 2);
 		floor++;
 	}
 }
