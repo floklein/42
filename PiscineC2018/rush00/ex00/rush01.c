d2f202d455a29834e23df1e1d210ce3150e44554
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-08-31 00:31:15 +0200

    on va retry sastantua + fini rush

---
+++ PiscineC2018/rush00/ex00/rush01.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/31 00:14:20 by flklein           #+#    #+#             */
+/*   Updated: 2018/08/31 00:16:06 by flklein          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	display(int x, int y, int a, int b)
+{
+	if (b == 1 && a == 1)
+		ft_putchar('/');
+	else if (b == 1 && a == x)
+		ft_putchar('\\');
+	else if (b == y && a == 1)
+		ft_putchar('\\');
+	else if (b == y && a == x)
+		ft_putchar('/');
+	else if (b == 1 || b == y)
+		ft_putchar('*');
+	else if (a == 1 || a == x)
+		ft_putchar('*');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int		a;
+	int		b;
+
+	b = 1;
+	if (!(x < 0 || y < 0))
+	{
+		while (b <= y)
+		{
+			a = 1;
+			while (a <= x)
+			{
+				display(x, y, a, b);
+				a++;
+			}
+			ft_putchar('\n');
+			b++;
+		}
+	}
+}
