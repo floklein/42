be7ddbc9c3b9cb3fd88b7e0753fd0e43320f2ac3
Florent KLEIN <flklein@e3r11p1.42.fr>
2018-09-02 03:54:02 +0200

    putnbrbase

--- PiscineC2018/j05/ex20/ft_putnbr_base.c
+++ PiscineC2018/j05/ex20/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/02 02:56:14 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/02 03:13:06 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/02 03:52:02 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,37 +20,40 @@ int		ft_strlen(char *str)
 	int		len;
 
 	len = 0;
-	while (str[len++]);
-	return (len - 1);
+	while (str[len])
+	{
+		if (str[len] == '+' || str[len] == '-')
+			return (0);
+		len++;
+	}
+	return (len);
 }
 
 void	ft_putnbr_base(int nb, char *base)
 {
 	int		base_n;
 
-	base_n = ft_strlen(base);
-//	printf("base:%d\n", base_n);
-	if (nb == -2147483648)
+	base_n = check_base(base);
+	if (base_n > 1)
 	{
-		ft_putchar('-');
-		ft_putchar('2');
-		nb = 147483648;
+		if (nb == -2147483648)
+		{}
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		if (nb >= base_n)
+		{
+			ft_putnbr_base(nb / base_n, base);
+			ft_putnbr_base(nb % base_n, base);
+		}
+		else
+			ft_putchar(base[nb]);
 	}
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-	}
-	if (nb >= base_n)
-	{
-		ft_putnbr_base(nb / base_n, base);
-		ft_putnbr_base(nb % base_n, base);
-	}
-	else
-		ft_putchar(base[nb]);
 }
 
 int		main()
 {
-	ft_putnbr_base(12345, "01234567");
+	ft_putnbr_base(-12345, "01234567");
 }
