0a59e8c87a5aec45c1a29b5f28fa7140b5c51b9c
Florent KLEIN <flklein@e3r10p8.42.fr>
2018-09-04 16:46:48 +0200

    rename

--- PiscineC2018/j05/ex23/ft_print_memory.c
+++ PiscineC2018/j05/ex23/ft_print_memory.c
@@ -6,13 +6,13 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 20:15:59 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/03 22:43:41 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/04 16:38:01 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_putnbr_16(int nbr, int size)
+void	ft_putnbr_16(int nbr)
 {
 	int		base_n;
 	char	*base;
@@ -21,8 +21,8 @@ void	ft_putnbr_16(int nbr, int size)
 	base = "0123456789abcdef";
 	if (nbr >= base_n)
 	{
-		ft_putnbr_16(nbr / base_n, size - 1);
-		ft_putnbr_16(nbr % base_n, 0);
+		ft_putnbr_16(nbr / base_n);
+		ft_putnbr_16(nbr % base_n);
 	}
 	else
 	{
@@ -30,35 +30,78 @@ void	ft_putnbr_16(int nbr, int size)
 	}
 }
 
-void	*ft_print_memory(char *addr, unsigned int size)
+void	ft_putnstr_np(char *str, int n)
 {
-	int		cur_add;
 	int		i;
 
-	while (cur_add <= size && addr[cur_add])
+	i = 0;
+	if (n == -1)
+		while (*(str++))
+			ft_putchar(*(str - 1));
+	else
 	{
-		ft_putnbr_16(cur_add, 8);
-		ft_putchar(':');
-		ft_putchar('\t');
-		i = cur_add;
-		while (i < cur_add + 16 && addr[i])
+		while (i < n)
 		{
-			ft_putnbr_16(addr[i], 2);
-			if (addr[i + 1])
-				ft_putnbr_16(addr[i + 1], 2);
-			ft_putchar('\t');
-			i += 2;
-		}
-		ft_putchar('\t');
-		i = cur_add;
-		while (i < cur_add + 16 && addr[i])
-		{
-			if (addr[i] >= 32 && addr[i] <= 126)
-				ft_putchar(addr[i]);
+			if (str[i] >= 32 && str[i] <= 126)
+				ft_putchar(str[i]);
 			else
 				ft_putchar('.');
 			i++;
 		}
+	}
+}
+
+void	print_0(int add)
+{
+	if (add < 16)
+		ft_putnstr_np("0000000", -1);
+	else if (add < 256)
+		ft_putnstr_np("000000", -1);
+	else if (add < 4096)
+		ft_putnstr_np("00000", -1);
+	else if (add < 65536)
+		ft_putnstr_np("0000", -1);
+	else if (add < 1048576)
+		ft_putnstr_np("000", -1);
+	else if (add < 268435456)
+		ft_putnstr_np("00", -1);
+	else
+		ft_putnstr_np("0", -1);
+}
+
+void	print_ascii(char *char_addr, int cur_add, int size, int i)
+{
+	if (i < size)
+	{
+		if (char_addr[i] < 16)
+			ft_putchar('0');
+		ft_putnbr_16(char_addr[i]);
+	}
+	else
+		ft_putnstr_np("  ", -1);
+	if (i % 2 && i < cur_add + 15)
+		ft_putchar(' ');
+}
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	char	*char_addr;
+	int		cur_add;
+	int		i;
+
+	char_addr = (char *)addr;
+	cur_add = 0;
+	while (cur_add <= size && char_addr[cur_add])
+	{
+		print_0(cur_add);
+		ft_putnbr_16(cur_add);
+		ft_putnstr_np(": ", -1);
+		i = cur_add;
+		while (i < cur_add + 16)
+			print_ascii(char_addr, cur_add, size, i++);
+		ft_putnstr_np("  ", -1);
+		i = cur_add;
+		ft_putnstr_np(char_addr + i, cur_add + 16 > size ? size - cur_add : 16);
 		ft_putchar('\n');
 		cur_add += 16;
 	}
@@ -70,7 +113,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int		main(int ac, char **av)
+int		main()
 {
-	ft_print_memory(av[1], atoi(av[2]));
+	ft_print_memory("", 0);
 }
