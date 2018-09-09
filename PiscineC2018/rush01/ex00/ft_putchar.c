5032ea3658fecf7a40a7d313751eaf840a128f68
Florent KLEIN <flklein@e2r4p22.42.fr>
2018-09-09 20:05:25 +0200

    rush

--- PiscineC2018/rush01/ex00/ft_putchar.c
+++ PiscineC2018/rush01/ex00/ft_putchar.c
@@ -6,11 +6,21 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 19:48:33 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/08 19:50:04 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/09 16:07:06 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sudoku.h"
+
+int		ft_strlen(char *str)
+{
+	int		len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
 
 void	ft_putchar(char c)
 {
--- PiscineC2018/rush01/ex00/sudoku.h
+++ PiscineC2018/rush01/ex00/sudoku.h
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 19:50:11 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/08 22:46:59 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/09 18:24:56 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 # include <stdlib.h>
 
 void	ft_putchar(char c);
+int		ft_strlen(char *str);
 int		**create_tab(char **av);
-int		**copy_tab(int **old);
+void	copy_tab(int **tab, int **old);
 void	print_tab(int **tab);
-int		test(int **tab, int pos, int nb);
+int		test(int **tab, int l, int c, int nb);
+int		is_valid(int **tab);
 
 #endif
