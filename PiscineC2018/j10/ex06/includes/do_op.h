3c11c29050a45d6c2f1f594696b4d0cd27f9ab61
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-11 00:01:35 +0200

    do op fini

--- PiscineC2018/j10/ex06/includes/do_op.h
+++ PiscineC2018/j10/ex06/includes/do_op.h
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 17:35:10 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/10 17:51:22 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/10 23:08:32 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,16 @@
 # include <unistd.h>
 
 void	ft_putchar(char c);
-void	ft_putstr(char *str);
+int		ft_putstr(char *str);
+int		ft_strlen(char *str);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strstr(char *str, char *to_find);
 int		ft_atoi(char *str);
 void	ft_putnbr(int nb);
-int		ft_str_is_numeric(char *str);
+int		ft_add(int nb1, int nb2);
+int		ft_sub(int nb1, int nb2);
+int		ft_mult(int nb1, int nb2);
+int		ft_div(int nb1, int nb2);
+int		ft_mod(int nb1, int nb2);
 
 #endif
--- PiscineC2018/j10/ex06/srcs/operations.c
+++ PiscineC2018/j10/ex06/srcs/operations.c
@@ -1,19 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/10 17:34:06 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/10 17:45:24 by flklein          ###   ########.fr       */
+/*   Created: 2018/09/10 18:44:51 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/10 20:12:45 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "do_op.h"
 
-void	ft_putstr(char *str)
+int		ft_add(int nb1, int nb2)
 {
-	while (*str)
-		ft_putchar(*(str++));
+	return (nb1 + nb2);
+}
+
+int		ft_sub(int nb1, int nb2)
+{
+	return (nb1 - nb2);
+}
+
+int		ft_mult(int nb1, int nb2)
+{
+	return (nb1 * nb2);
+}
+
+int		ft_div(int nb1, int nb2)
+{
+	return (nb1 / nb2);
+}
+
+int		ft_mod(int nb1, int nb2)
+{
+	return (nb1 % nb2);
 }
