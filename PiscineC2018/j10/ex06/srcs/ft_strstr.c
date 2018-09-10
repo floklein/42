02ec217b9ff1d1dd08b849f5e3ce247f6f81383a
Florent KLEIN <flklein@e3r11p11.42.fr>
2018-09-11 00:01:35 +0200

    do op fini

--- PiscineC2018/j10/ex06/srcs/ft_strstr.c
+++ PiscineC2018/j10/ex06/srcs/ft_strstr.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/02 00:58:51 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/10 17:50:25 by flklein          ###   ########.fr       */
+/*   Created: 2018/09/01 23:20:41 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/04 15:49:04 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "do_op.h"
+#include <stdlib.h>
 
-int		ft_str_is_numeric(char *str)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int		i;
+	int		j;
 
 	i = 0;
+	if (!to_find)
+		return (str);
 	while (str[i])
 	{
-		if (!(str[i] >= '0' && str[i] <= '9'))
-			return (0);
+		j = 0;
+		while (str[i + j] && to_find[j] && str[i + j] == to_find[j])
+			j++;
+		if (!to_find[j])
+			return (str + i);
 		i++;
 	}
-	return (1);
+	return (NULL);
 }
