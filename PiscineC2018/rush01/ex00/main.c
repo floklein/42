dde7031f657d3b38cd347cd85ba464223688acdd
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-11 00:29:54 +0200

    sudoku commente

--- PiscineC2018/rush01/ex00/main.c
+++ PiscineC2018/rush01/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 19:17:17 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/09 21:57:42 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/11 00:25:23 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int		this_is_the_end(int **tab, int **tab_cpy, int *res)
 {
-	if (*res == 0)
+	if (*res == 0) //1er resulat -> on fait une copie du tableau
 		copy_tab(tab_cpy, tab);
 	(*res)++;
-	if (*res == 2)
+	if (*res == 2) //2eme -> c'est trop = erreur
 		return (1);
 	return (0);
 }
@@ -28,24 +28,24 @@ int		solve_sudoku(int **tab, int **tab_cpy, int *res, int pos)
 	int		c;
 	int		nb;
 
-	if (pos == 81)
+	if (pos == 81) //Condition de fin : derniere position de la grille
 		return (this_is_the_end(tab, tab_cpy, res));
-	l = pos / 9;
-	c = pos % 9;
-	if (tab[l][c] != 0)
+	l = pos / 9; //Ligne
+	c = pos % 9; //Colonne
+	if (tab[l][c] != 0) //Si un chiffre deja ecrit -> case suivante
 		return (solve_sudoku(tab, tab_cpy, res, pos + 1));
 	nb = 1;
-	while (nb <= 9)
+	while (nb <= 9) //Test tous les nbs possibles
 	{
-		if (test(tab, l, c, nb))
+		if (test(tab, l, c, nb)) //Nb respecte conditions ?
 		{
-			tab[l][c] = nb;
-			if (solve_sudoku(tab, tab_cpy, res, pos + 1))
+			tab[l][c] = nb; //Oui -> on l'ecrit
+			if (solve_sudoku(tab, tab_cpy, res, pos + 1)) //Case suivante
 				return (1);
 		}
-		nb++;
+		nb++; //Non -> nombre suivant
 	}
-	tab[l][c] = 0;
+	tab[l][c] = 0; //Aucun nb fonctionne -> on remet a 0 la case
 	return (0);
 }
 
@@ -55,15 +55,15 @@ int		main(int ac, char **av)
 	int		**tab_cpy;
 	int		res;
 
-	if (ac != 10 || !(tab = create_tab(av))
-			|| !(tab_cpy = create_tab(av)) || !(is_valid(tab)))
+	if (ac != 10 || !(tab = create_tab(av)) //Verifie le nombre de lignes
+			|| !(tab_cpy = create_tab(av)) || !(is_valid(tab))) //Sudoku valide ?
 	{
 		write(1, "Error\n", 6);
 		return (0);
 	}
-	solve_sudoku(tab, tab_cpy, &res, 0);
+	solve_sudoku(tab, tab_cpy, &res, 0); //Appel du solver
 	if (res == 1)
-		print_tab(tab_cpy);
+		print_tab(tab_cpy); //Affiche le resultat unique
 	else
 		write(1, "Error\n", 6);
 	return (0);
