d033d87197a181690273a967a7d42debd5d07e5d
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-10 17:52:28 +0200

    do op

---
+++ PiscineC2018/j10/ex06/do_op.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_op.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/10 17:35:10 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/10 17:51:22 by flklein          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DO_OP_H
+# define DO_OP_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_atoi(char *str);
+void	ft_putnbr(int nb);
+int		ft_str_is_numeric(char *str);
+
+#endif
