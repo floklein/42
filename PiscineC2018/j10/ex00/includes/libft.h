f4fe0f2e8e187284ba9efcd649de1c8841c18cb1
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-11 02:52:19 +0200

    better makefiles

--- PiscineC2018/j10/ex00/includes/libft.h
+++ PiscineC2018/j10/ex00/includes/libft.h
@@ -1,17 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/07 00:19:27 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/07 00:22:43 by flklein          ###   ########.fr       */
+/*   Created: 2018/09/11 01:40:01 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/11 01:42:19 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
