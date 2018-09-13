b87345a88669a766e0805f62260867c2b9c86481
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-13 22:21:52 +0200

    reverse fun

--- PiscineC2018/j11/ex14/ft_list_sort.c
+++ PiscineC2018/j11/ex14/ft_list_sort.c
@@ -1,36 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*   ft_list_sort.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/13 11:33:02 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/13 19:15:53 by flklein          ###   ########.fr       */
+/*   Created: 2018/09/13 15:20:52 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/13 17:06:35 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_reverse(t_list **begin_list)
+void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
 	t_list	*tmp1;
 	t_list	*tmp2;
 	t_list	*tmp3;
+	void	*min;
 
-	if (!(*begin_list) || !((*begin_list)->next))
-		return ;
 	tmp1 = *begin_list;
-	tmp2 = tmp1->next;
-	tmp3 = tmp2->next;
-	tmp1->next = NULL;
-	tmp2->next = tmp1;
-	while (tmp3)
+	while (tmp1)
 	{
-		tmp1 = tmp2;
-		tmp2 = tmp3;
-		tmp3 = tmp3->next;
-		tmp2->next = tmp1;
+		tmp3 = tmp1;
+		min = tmp1->data;
+		tmp2 = tmp1->next;
+		while (tmp2)
+		{
+			if ((*cmp)(min, tmp2->data) > 0)
+			{
+				tmp3 = tmp2;
+				min = tmp3->data;
+			}
+			tmp2 = tmp2->next;
+		}
+		tmp3->data = tmp1->data;
+		tmp1->data = min;
+		tmp1 = tmp1->next;
 	}
-	*begin_list = tmp2;
 }
