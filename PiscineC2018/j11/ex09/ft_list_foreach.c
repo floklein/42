40dab6ca05d9e9c43b54a6459a4a0e7873eee164
Florent KLEIN <4128942+floklein@users.noreply.github.com>
2018-09-13 13:04:24 +0200

    j11ex12

---
+++ PiscineC2018/j11/ex09/ft_list_foreach.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/13 12:32:37 by flklein           #+#    #+#             */
+/*   Updated: 2018/09/13 12:37:45 by flklein          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	if (!begin_list)
+		return ;
+	(*f)(begin_list->data);
+	if (begin_list->next)
+		ft_list_foreach(begin_list->next, f);
+}
