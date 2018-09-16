2c876e94cd457bf79384941941b23f8b434096de
Florent KLEIN <flklein@e3r10p9.42.fr>
2018-09-16 22:05:29 +0200

    eval expr FINI

--- PiscineC2018/evalexpr/ex00/srcs/parse.c
+++ PiscineC2018/evalexpr/ex00/srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 17:11:04 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/16 20:43:17 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/16 21:15:37 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,43 +33,48 @@ char	*kill_spaces(char *src)
 	return (dest);
 }
 
-int		is_operand(char ope)
+int		is_op(char ope)
 {
 	return (ope == '+' || ope == '-' || ope == '*' || ope == '/' || ope == '%');
+}
+
+void	parse_helper(char ***tab, char *str)
+{
+	int		i;
+	int		k;
+	int		j;
+
+	i = 0;
+	k = 0;
+	while (str[i])
+	{
+		if (!((*tab)[k] = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
+			return ;
+		j = 0;
+		if (str[i] == '(' || str[i] == ')')
+			(*tab)[k++][0] = str[i++];
+		else if (is_op(str[i]) && !is_op(str[i - 1]) && str[i - 1] != '(')
+			(*tab)[k++][0] = str[i++];
+		else
+		{
+			if (is_op(str[i]))
+				(*tab)[k][j++] = str[i++];
+			while (str[i] >= '0' && str[i] <= '9')
+				(*tab)[k][j++] = str[i++];
+			k++;
+		}
+	}
+	(*tab)[k] = NULL;
 }
 
 char	**parse_str(char *src)
 {
 	char	*str;
 	char	**tab;
-	int		i;
-	int		k;
-	int		j;
 
 	str = kill_spaces(src);
 	if (!(tab = (char **)malloc(sizeof(char *) * (ft_strlen(str) + 1))))
 		return (NULL);
-	i = 0;
-	k = 0;
-	while (str[i])
-	{
-		if (!(tab[k] = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
-			return (NULL);
-		j = 0;
-		if (str[i] == '(' || str[i] == ')')
-			tab[k++][0] = str[i++];
-		else if (is_operand(str[i]) && !is_operand(str[i - 1])
-					&& str[i - 1] != '(')
-			tab[k++][0] = str[i++];
-		else
-		{
-			if (is_operand(str[i]))
-				tab[k][j++] = str[i++];
-			while (str[i] >= '0' && str[i] <= '9')
-				tab[k][j++] = str[i++];
-			k++;
-		}
-	}
-	tab[k] = NULL;
+	parse_helper(&tab, str);
 	return (tab);
 }
