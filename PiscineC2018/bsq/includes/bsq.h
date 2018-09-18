df574d1586ca5b1a0cd73138c208c7c2867c715f
Florent KLEIN <flklein@e3r10p10.42.fr>
2018-09-18 14:38:34 +0200

    bsq params

--- PiscineC2018/bsq/includes/bsq.h
+++ PiscineC2018/bsq/includes/bsq.h
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 11:50:59 by flklein           #+#    #+#             */
-/*   Updated: 2018/09/18 12:18:49 by flklein          ###   ########.fr       */
+/*   Updated: 2018/09/18 14:37:37 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,23 @@
 
 # define BUF_SIZE 4096
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putchar_err(char c);
-void	ft_putstr_err(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-char	*ft_strjoin(char *s1, char *s2);
+typedef struct	s_params
+{
+	char	*map;
+	int		lines;
+	char	p;
+	char	o;
+	char	x;
+}				t_params;
+
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		ft_putchar_err(char c);
+void		ft_putstr_err(char *str);
+int			ft_strlen(char *str);
+int			ft_strcmp(char *s1, char *s2);
+char		*ft_strjoin(char *s1, char *s2);
+int			ft_atoi(char *str);
+int			parse_map(t_params *par, char *map);
 
 #endif
