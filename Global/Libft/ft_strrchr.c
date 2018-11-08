041a0ada27e7cc88d31285d83325c0169391b12b
Florent Klein <flklein@student.42.fr>
2018-11-08 13:03:02 +0100

    libft

--- Global/Libft/ft_strrchr.c
+++ Global/Libft/ft_strrchr.c
@@ -1,32 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/05 19:01:36 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/08 12:39:33 by flklein          ###   ########.fr       */
+/*   Created: 2018/11/08 12:44:07 by flklein           #+#    #+#             */
+/*   Updated: 2018/11/08 12:45:11 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_strrchr(const char *s, int c)
 {
-	char	*dest;
-	int		i;
+	char	*tmp;
+	size_t	i;
 
-	if (!src)
-		return (NULL);
-	if (!(dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char))))
-		return (NULL);
+	tmp = NULL;
 	i = 0;
-	while (src[i])
+	while (s[i])
 	{
-		dest[i] = src[i];
+		if (s[i] == c)
+			tmp = (char *)s + i;
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	if (s[i] == c)
+		tmp = (char *)s + i;
+	return (tmp);
 }
