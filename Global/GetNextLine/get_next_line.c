4e8b7d20857ad3e1c2aa0bd17834baa2158de9d1
Florent Klein <4128942+floklein@users.noreply.github.com>
2018-11-19 15:37:52 +0100

    FINI

--- Global/GetNextLine/get_next_line.c
+++ Global/GetNextLine/get_next_line.c
@@ -6,137 +6,107 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:54:03 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/15 19:29:32 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/19 15:37:01 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_file	*ft_newfile(int fd)
+t_file			*ft_newfile(int fd)
 {
-	t_file	*file;
+	t_file		*file;
 
 	if (!(file = (t_file*)malloc(sizeof(t_file))))
 		return (NULL);
 	file->next = NULL;
 	file->fd = fd;
-	file->buf = NULL;
+	file->data = NULL;
 	return (file);
 }
 
-char	**ft_getfile(t_file **file, int fd)
+t_file			*ft_srchfile(t_file *file, int fd)
 {
-	if (*file)
+	if (file)
 	{
-		if ((*file)->fd == fd)
-			return (&((*file)->buf));
-		else if ((*file)->next)
-			return (ft_getfile(&((*file)->next), fd));
-		else
-		{
-			(*file)->next = ft_newfile(fd);
-			return (&((*file)->next->buf));
-		}
+		if (file->fd == fd)
+			return (file);
+		else if (file->next)
+			return (ft_srchfile(file->next, fd));
+		file->next = ft_newfile(fd);
+		return (file->next);
 	}
-	else
-	{
-		*file = ft_newfile(fd);
-		return (&((*file)->buf));
-	}
+	if (!(file = (t_file*)malloc(sizeof(t_file))))
+		return (NULL);
+	file = ft_newfile(fd);
+	return (file);
 }
 
-char	ft_treatbuffer(char **line, char **buf)
+int				ft_delelem(t_file **file, int fd, int ret)
 {
-	int		i;
-	int		rest;
+	t_file		*last;
+	t_file		*tofree;
 
-	rest = 0;
-	if (**buf == -1)
-	{
-		rest = 1;
-		(*buf)++;
-	}
-	i = 0;
-	while ((*buf)[i] && (*buf)[i] != '\n')
-		i++;
-	if ((*buf)[i] == '\n')
-	{
-		*line = ft_strjoin(*line, ft_strsub(*buf, 0, i));
-		*buf += i;
-		**buf = -1;
-		printf("\\n\n");
-		return ('\n');
-	}
-	else if ((*buf)[i] == '\0' && ft_strlen(*buf) <= BUFF_SIZE && rest == 1)
-	{
-		*line = ft_strjoin(*line, *buf);
-		printf("REST\n");
-		return ('R');
-	}
-	else if ((*buf)[i] == '\0' && ft_strlen(*buf) < BUFF_SIZE && rest == 0)
-	{
-		*line = ft_strjoin(*line, *buf);
-		ft_strclr(*buf);
-		printf("EOF\n");
-		return ('E');
-	}
+	last = *file;
+	while (last->fd != fd && last->next && last->next->fd != fd)
+		last = last->next;
+	tofree = last->fd == fd ? last : last->next;
+	if (last->fd == fd)
+		*file = tofree->next;
 	else
-	{
-		*line = ft_strjoin(*line, *buf);
-		*buf = ft_strnew(BUFF_SIZE);
-		printf("READ\n");
-		return (0);
-	}
+		last->next = tofree->next;
+	ft_strdel(&(tofree->data));
+	free(tofree);
+	return (ret);
 }
 
-int		get_next_line(const int fd, char **line)
+int				ft_treat(char **data, char **line)
 {
-	int				r;
+	char	*tmp;
+	int		l_line;
+
+	l_line = 0;
+	while ((*data)[l_line] && (*data)[l_line] != '\n')
+		l_line++;
+	if (((*data)[l_line]) == '\n')
+	{
+		*line = ft_strsub(*data, 0, l_line);
+		tmp = *data;
+		*data = ft_strdup(&(*data)[l_line + 1]);
+		free(tmp);
+		!(*data)[0] ? ft_strdel(data) : 0;
+	}
+	else if (!((*data)[l_line]))
+	{
+		*line = ft_strdup(*data);
+		ft_strdel(data);
+	}
+	return (1);
+}
+
+int				get_next_line(const int fd, char **line)
+{
 	static t_file	*file = NULL;
-	char			**buf;
-	int				output;
+	t_file			*cur;
+	char			*tmp;
+	char			*buf;
+	int				r;
 
-	if (fd < 0 || !line || !(*line = ft_strnew(BUFF_SIZE)))
+	if (fd < 0 || !line || (!file && !(file = ft_newfile(fd))))
 		return (-1);
-	if (!file)
-		if (!(file = ft_newfile(fd)))
-			return (-1);
-	buf = ft_getfile(&file, fd);
-	if (*buf)
-	{
-		output = ft_treatbuffer(line, buf);
-		if (output == '\n')
-			return (1);
-		else if (output == 'E')
-			return (0);
-	}
-	if (!(*buf = ft_strnew(BUFF_SIZE)))
+	cur = ft_srchfile(file, fd);
+	if ((!(cur->data) && !(cur->data = ft_strnew(0))) ||
+			!(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	while ((r = read(fd, *buf, BUFF_SIZE)))
+	while (!(r = 0) && !ft_strchr(cur->data, '\n') && !ft_strchr(buf, '\n')
+			&& (r = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (r < 0)
-			return (-1);
-		(*buf)[r] = '\0';
-		if (ft_treatbuffer(line, buf) > 0)
-			return (1);
+		buf[r] = 0;
+		tmp = cur->data;
+		cur->data = ft_strjoin(cur->data, buf);
+		free(tmp);
 	}
-	return (*line != NULL);
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-
-int		main(int ac, char **av)
-{
-	char	*line;
-	int		fd;
-	int		ret;
-
-	fd = open(av[1], O_RDONLY);
-	while (ac++ < 15)
-	{
-		ret = get_next_line(fd, &line);
-		printf("%s\n	->%d\n", line, ret);
-	}
-	return (ac);
+	free(buf);
+	if (r < 0 || (r == 0 && (!(cur->data) || !(cur->data)[0])))
+		return (ft_delelem(&file, fd, r < 0 ? -1 : 0));
+	return (ft_treat(&(cur->data), line));
 }
