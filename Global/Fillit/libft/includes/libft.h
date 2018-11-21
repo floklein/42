ac12c6ec1771aee92229626abc938123b3078449
Florent Klein <flklein@student.42.fr>
2018-11-21 20:58:30 +0100

    parse termine

--- Global/Fillit/libft/includes/libft.h
+++ Global/Fillit/libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: flklein <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:22:08 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/20 18:09:53 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/21 20:43:16 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,5 +104,6 @@ void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int				get_next_line(int fd, char **line);
+int				ft_putstr_int(char const *str, int ret);
 
 #endif
--- Global/Fillit/srcs/main.c
+++ Global/Fillit/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 17:03:58 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/21 16:24:12 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/21 20:54:16 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,83 @@
 
 #include <stdio.h>
 
-int		ft_charvalid(char *str)
+int		ft_valid_piece(char *str)
 {
 	int		i;
+	int		links;
+	int		hashes;
 
+	links = 0;
+	hashes = 0;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] != '#' && str[i] != '.')
+		if ((i + 1) % 5 == 0 && str[i] != '\n')
 			return (0);
+		if ((i + 1) % 5 != 0 && str[i] != '#' && str[i] != '.')
+			return (0);
+		if (str[i] == '#')
+		{
+			hashes++;
+			links += (i + 1 <= 20 ? str[i + 1] == '#' : 0)
+				+ (i - 1 >= 0 ? str[i - 1] == '#' : 0)
+				+ (i + 5 <= 20 ? str[i + 5] == '#' : 0)
+				+ (i - 5 >= 0 ? str[i - 5] == '#' : 0);
+		}
 		i++;
 	}
-	return (1);
+	return (hashes == 4 && links >= 6);
+}
+
+char	*ft_convert(char *src, char c)
+{
+	char	*dst;
+	int		i;
+	int		j;
+
+	if (!(dst = ft_strnew(16)))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (src[i])
+	{
+		if (src[i] == '.')
+			dst[j++] = src[i];
+		else if (src[i] == '#')
+			dst[j++] = c;
+		i++;
+	}
+	dst[j] = '\0';
+	return (dst);
 }
 
 t_tetri	*ft_parse(int fd)
 {
-	t_tetri	*start;
+	t_tetri	*tetri;
 	int		r;
-	char	*line;
-	int		num_l;
-	char	*stock;
+	char	*buf;
+	char	*tmp;
+	char	pieces;
 
-	stock = ft_strnew(0);
-	num_l = 0;
-	while ((r = get_next_line(fd, &line)))
+	if (!(buf = ft_strnew(20)))
+		return (NULL);
+	pieces = 'A' - 1;
+	r = 1;
+	while (r && ++pieces <= 'Z')
 	{
-		if (!ft_strequ(line, "\0"))
-		{
-			stock = ft_strjoin(stock, line);
-			if (ft_strlen(line) != 4 || !ft_charvalid(line))
-				return (NULL);
-			num_l++;
-		}
-		else
-		{
-			if (num_l != 4)
-				return (NULL);
-			printf("stock:%s\n", stock);
-			ft_list_push_back(&start, stock);
-			stock = ft_strnew(0);
-			num_l = 0;
-		}
+		if ((r = read(fd, buf, 20)) < 0)
+			return (NULL);
+		buf[r] = '\0';
+		if (r != 20 || !ft_valid_piece(buf) || !(tmp = ft_convert(buf, pieces)))
+			return (NULL);
+		ft_list_push_back(&tetri, tmp);
+		if ((r = read(fd, buf, 1)) < 0)
+			return (NULL);
+		buf[r] = '\0';
+		if (r == 1 && !ft_strequ(buf, "\n"))
+			return (NULL);
 	}
-	return (start);
+	return (pieces <= 'Z' ? tetri : NULL);
 }
 
 int		main(int ac, char **av)
@@ -65,9 +98,14 @@ int		main(int ac, char **av)
 	int		fd;
 	t_tetri	*tetri;
 
-	fd = open(av[1], O_RDONLY);
-	tetri = ft_parse(fd);
+	if (ac != 2)
+		return (ft_putstr_int("usage\n", 0));
+	if ((fd = open(av[1], O_RDONLY)) < 0)
+		return (ft_putstr_int("error\n", 0));
+	if (!(tetri = ft_parse(fd)))
+		return (ft_putstr_int("error\n", 0));
+	ft_putendl("TETRI:");
 	ft_list_foreach(tetri, &ft_putendl);
 	close(fd);
-	return (ac);
+	return (0);
 }
