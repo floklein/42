4554753877aabedbf7ce26a53794db1b26309715
Florent Klein <4128942+floklein@users.noreply.github.com>
2018-11-23 19:44:53 +0100

    reste les leaks lol

--- Global/Fillit/srcs/main.c
+++ Global/Fillit/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 18:23:17 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/23 18:23:26 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/23 19:32:58 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ int		main(int ac, char **av)
 	t_map	*map;
 
 	if (ac != 2)
-		return (ft_putstr_int("usage\n", 0));
+		return (ft_putstr_int("usage: ./fillit <file>\n", 0));
 	if ((fd = open(av[1], O_RDONLY)) < 0)
 		return (ft_putstr_int("error\n", 0));
 	if (!(tetri = ft_parse(fd)))
 		return (ft_putstr_int("error\n", 0));
-	map = ft_solve(tetri);
+	if (!(map = ft_solve(tetri)))
+		return (ft_putstr_int("error\n", 0));
 	ft_putstr(map->box);
 	close(fd);
 	return (0);
