b21e3beb397f10e939d35e5dbb0a82ab694bc495
Florent Klein <flo-klein@hotmail.fr>
2018-11-27 00:38:38 +0100

    on affiche des trucs

--- Graphics/FdF/src/setup.c
+++ Graphics/FdF/src/setup.c
@@ -1,40 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   setup.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 12:23:42 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/27 00:34:51 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/27 00:08:08 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int		main(void)
+void	ft_img_setup(t_mlx *mlx)
+{
+	int		bpp;
+	int		s_l;
+	int		endian;
+
+	mlx->img = mlx_new_image(mlx->ptr, mlx->width, mlx->height);
+	mlx->data = (int *)mlx_get_data_addr(mlx->img, &bpp, &s_l, &endian);
+}
+
+t_mlx	*ft_mlx_setup(int width, int height)
 {
 	t_mlx	*mlx;
-//	int		x;
-//	int		y;
 
-	mlx = ft_mlx_setup(1200, 800);
-	ft_img_setup(mlx);
-//	x = 0;
-//	while (++x < mlx->width)
-//	{
-//		y = 0;
-//		while (++y < mlx->height)
-//		{
-//			if (y % 20 < 10)
-//				ft_fill_pixel(mlx, x, y, x % 20 < 10 ? 0xFF0000 : 0x00FF00);
-//			else
-//				ft_fill_pixel(mlx, x, y, x % 20 < 10 ? 0x0000FF : 0x0F0F0F);
-//		}
-//	}
-	ft_line(mlx, 50, 50, 1000, 800);
-	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img, 0, 0);
-	mlx_key_hook(mlx->win, &ft_key, mlx);
-	mlx_loop(mlx->ptr);
-	return (0);
+	if (!(mlx = (t_mlx *)malloc(sizeof(t_mlx))))
+		return (NULL);
+	mlx->ptr = mlx_init();
+	mlx->width = width;
+	mlx->height = height;
+	mlx->win = mlx_new_window(mlx->ptr, mlx->width, mlx->height, "fdf");
+	return (mlx);
 }
