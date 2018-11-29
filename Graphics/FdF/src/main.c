bcfecac9f65edcde87372906889ba4e59fa13577
Florent Klein <flo-klein@hotmail.fr>
2018-11-29 22:41:20 +0100

    debut display

--- Graphics/FdF/src/main.c
+++ Graphics/FdF/src/main.c
@@ -6,7 +6,7 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 12:23:42 by flklein           #+#    #+#             */
-/*   Updated: 2018/11/29 21:42:30 by flklein          ###   ########.fr       */
+/*   Updated: 2018/11/29 22:17:19 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int		main(int ac, char **av)
 {
 	t_map	*map;
 	t_mlx	*mlx;
-	t_coord	*coord;
 
 	if (ac != 2)
 		return (ft_usage());
@@ -32,13 +31,7 @@ int		main(int ac, char **av)
 	if (!(mlx = ft_mlx_setup(1200, 800)))
 		return (0);
 	ft_img_setup(mlx);
-	if (!(coord = (t_coord *)malloc(sizeof(t_coord))))
-		return (0);
-	coord->x1 = 50;
-	coord->y1 = 50;
-	coord->x2 = 500;
-	coord->y2 = 250;
-	ft_line(mlx, coord, 0xFFFF00);
+	ft_put_map_to_img(mlx, map);
 	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img, 0, 0);
 	mlx_key_hook(mlx->win, &ft_key, mlx);
 	mlx_loop(mlx->ptr);
