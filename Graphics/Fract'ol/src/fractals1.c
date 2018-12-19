4c40621af88f3f0cde3c4cad5013e5a764e219f4
Florent Klein <4128942+floklein@users.noreply.github.com>
2018-12-19 16:16:09 +0100

    on norme encore

--- Graphics/Fract'ol/src/fractals1.c
+++ Graphics/Fract'ol/src/fractals1.c
@@ -6,31 +6,32 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 18:58:45 by flklein           #+#    #+#             */
-/*   Updated: 2018/12/19 15:45:34 by flklein          ###   ########.fr       */
+/*   Updated: 2018/12/19 16:10:31 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void	ft_mandelbrot_helper(t_thread *t, t_coord c, t_complex *new)
+static void	ft_mandelbrot_helper(t_thread *t, t_coord c)
 {
 	t_complex	p;
+	t_complex	new;
 	t_complex	old;
 
 	p.x = 1.5 * (c.x - t->mlx->width / 2)
 		/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x;
 	p.y = (c.y - t->mlx->height / 2)
 		/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
-	new->x = 0;
-	new->y = 0;
+	new.x = 0;
+	new.y = 0;
 	t->i = 0;
 	while (t->i < t->mlx->iter)
 	{
-		old.x = new->x;
-		old.y = new->y;
-		new->x = old.x * old.x - old.y * old.y + p.x;
-		new->y = 2 * old.x * old.y + p.y;
-		if (new->x * new->x + new->y * new->y > 4)
+		old.x = new.x;
+		old.y = new.y;
+		new.x = old.x * old.x - old.y * old.y + p.x;
+		new.y = 2 * old.x * old.y + p.y;
+		if (new.x * new.x + new.y * new.y > 4)
 			break ;
 		t->i++;
 	}
@@ -39,7 +40,6 @@ static void	ft_mandelbrot_helper(t_thread *t, t_coord c, t_complex *new)
 void		*ft_mandelbrot(void *threadv)
 {
 	t_coord		c;
-	t_complex	new;
 	t_thread	*t;
 
 	t = (t_thread *)threadv;
@@ -50,7 +50,7 @@ void		*ft_mandelbrot(void *threadv)
 		c.x = 0;
 		while (c.x < t->mlx->width)
 		{
-			ft_mandelbrot_helper(t, c, &new);
+			ft_mandelbrot_helper(t, c);
 			ft_fill_pixel(t->mlx, c.x, c.y, ft_hsv_to_rgb(t->i / 255.0, 1,
 						t->i < t->mlx->iter, t->mlx->panel_choice));
 			c.x++;
@@ -61,23 +61,23 @@ void		*ft_mandelbrot(void *threadv)
 	return (NULL);
 }
 
-static void	ft_julia_helper(t_thread *t, t_coord c, t_complex *new,
-		t_complex cst)
+static void	ft_julia_helper(t_thread *t, t_coord c, t_complex cst)
 {
+	t_complex	new;
 	t_complex	old;
 
-	new->x = 1.5 * (c.x - t->mlx->width / 2)
+	new.x = 1.5 * (c.x - t->mlx->width / 2)
 		/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x + 0.5;
-	new->y = (c.y - t->mlx->height / 2)
+	new.y = (c.y - t->mlx->height / 2)
 		/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
 	t->i = 0;
 	while (t->i < t->mlx->iter)
 	{
-		old.x = new->x;
-		old.y = new->y;
-		new->x = old.x * old.x - old.y * old.y + cst.x;
-		new->y = 2 * old.x * old.y + cst.y;
-		if (new->x * new->x + new->y * new->y > 4)
+		old.x = new.x;
+		old.y = new.y;
+		new.x = old.x * old.x - old.y * old.y + cst.x;
+		new.y = 2 * old.x * old.y + cst.y;
+		if (new.x * new.x + new.y * new.y > 4)
 			break ;
 		t->i++;
 	}
@@ -87,7 +87,6 @@ void		*ft_julia(void *threadv)
 {
 	t_coord		c;
 	t_complex	cst;
-	t_complex	new;
 	t_thread	*t;
 
 	t = (t_thread *)threadv;
@@ -100,7 +99,7 @@ void		*ft_julia(void *threadv)
 		c.x = 0;
 		while (c.x < t->mlx->width)
 		{
-			ft_julia_helper(t, c, &new, cst);
+			ft_julia_helper(t, c, cst);
 			ft_fill_pixel(t->mlx, c.x, c.y, ft_hsv_to_rgb(t->i / 255.0, 1,
 						t->i < t->mlx->iter, t->mlx->panel_choice));
 			c.x++;
--- Graphics/Fract'ol/src/fractals2.c
+++ Graphics/Fract'ol/src/fractals2.c
@@ -6,18 +6,40 @@
 /*   By: flklein <flklein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 16:38:43 by flklein           #+#    #+#             */
-/*   Updated: 2018/12/19 15:17:50 by flklein          ###   ########.fr       */
+/*   Updated: 2018/12/19 16:15:27 by flklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	*ft_tricorn(void *threadv)
+static void	ft_tricorn_helper(t_thread *t, t_coord c)
 {
-	t_coord		c;
 	t_complex	p;
 	t_complex	new;
 	t_complex	old;
+
+	p.x = 1.5 * (c.x - t->mlx->width / 2)
+		/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x;
+	p.y = (c.y - t->mlx->height / 2)
+		/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
+	new.x = 0;
+	new.y = 0;
+	t->i = 0;
+	while (t->i < t->mlx->iter)
+	{
+		old.x = new.x;
+		old.y = new.y;
+		new.x = old.x * old.x - old.y * old.y + p.x;
+		new.y = -2 * old.x * old.y + p.y;
+		if (new.x * new.x + new.y * new.y > 4)
+			break ;
+		t->i++;
+	}
+}
+
+void		*ft_tricorn(void *threadv)
+{
+	t_coord		c;
 	t_thread	*t;
 
 	t = (t_thread *)threadv;
@@ -28,26 +50,9 @@ void	*ft_tricorn(void *threadv)
 		c.x = 0;
 		while (c.x < t->mlx->width)
 		{
-			p.x = 1.5 * (c.x - t->mlx->width / 2)
-				/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x;
-			p.y = (c.y - t->mlx->height / 2)
-				/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
-			new.x = 0;
-			new.y = 0;
-			t->i = 0;
-			while (t->i < t->mlx->iter)
-			{
-				old.x = new.x;
-				old.y = new.y;
-				new.x = old.x * old.x - old.y * old.y + p.x;
-				new.y = -2 * old.x * old.y + p.y;
-				if (new.x * new.x + new.y * new.y > 4)
-					break ;
-				t->i++;
-			}
-			c.color = ft_hsv_to_rgb(t->i / 255.0, 1, t->i < t->mlx->iter,
-					t->mlx->panel_choice);
-			ft_fill_pixel(t->mlx, c.x, c.y, c.color);
+			ft_tricorn_helper(t, c);
+			ft_fill_pixel(t->mlx, c.x, c.y, ft_hsv_to_rgb(t->i / 255.0, 1,
+						t->i < t->mlx->iter, t->mlx->panel_choice));
 			c.x++;
 		}
 		c.y++;
@@ -56,12 +61,32 @@ void	*ft_tricorn(void *threadv)
 	return (NULL);
 }
 
-void	*ft_thunder(void *threadv)
+static void	ft_thunder_helper(t_thread *t, t_coord c, t_complex cst)
+{
+	t_complex	new;
+	t_complex	old;
+
+	new.x = 1.5 * (c.x - t->mlx->width / 2)
+		/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x + 0.5;
+	new.y = (c.y - t->mlx->height / 2)
+		/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
+	t->i = 0;
+	while (t->i < t->mlx->iter)
+	{
+		old.x = new.x;
+		old.y = new.y;
+		new.x = old.x * old.x - old.y * old.y + cst.x;
+		new.y = 2 * old.x * old.y + cst.y;
+		if (new.x * new.x + new.y * new.y > 4)
+			break ;
+		t->i++;
+	}
+}
+
+void		*ft_thunder(void *threadv)
 {
 	t_coord		c;
 	t_complex	cst;
-	t_complex	new;
-	t_complex	old;
 	t_thread	*t;
 
 	t = (t_thread *)threadv;
@@ -74,24 +99,9 @@ void	*ft_thunder(void *threadv)
 		c.x = 0;
 		while (c.x < t->mlx->width)
 		{
-			new.x = 1.5 * (c.x - t->mlx->width / 2)
-				/ (0.5 * t->mlx->zoom * t->mlx->width) + t->mlx->move_x + 0.5;
-			new.y = (c.y - t->mlx->height / 2)
-				/ (0.5 * t->mlx->zoom * t->mlx->height) + t->mlx->move_y;
-			t->i = 0;
-			while (t->i < t->mlx->iter)
-			{
-				old.x = new.x;
-				old.y = new.y;
-				new.x = old.x * old.x - old.y * old.y + cst.x;
-				new.y = 2 * old.x * old.y + cst.y;
-				if (new.x * new.x + new.y * new.y > 4)
-					break ;
-				t->i++;
-			}
-			c.color = ft_hsv_to_rgb(t->i / 255.0, 1, t->i < t->mlx->iter,
-					t->mlx->panel_choice);
-			ft_fill_pixel(t->mlx, c.x, c.y, c.color);
+			ft_thunder_helper(t, c, cst);
+			ft_fill_pixel(t->mlx, c.x, c.y, ft_hsv_to_rgb(t->i / 255.0, 1,
+						t->i < t->mlx->iter, t->mlx->panel_choice));
 			c.x++;
 		}
 		c.y++;
