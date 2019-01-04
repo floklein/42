7b48672d156e8a35a084f2f6e9f873849230551f
Florent Klein <flklein@student.42.fr>
2019-01-04 21:05:57 +0100

    deploy

---
+++ Network/RogerSkyline1/deploy.sh
@@ -0,0 +1,67 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    deploy.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: flklein <flklein@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/01/04 19:04:43 by flklein           #+#    #+#              #
+#    Updated: 2019/01/04 21:01:20 by flklein          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Port forwarding in VirtualBox:
+# ssh		3022		22
+# https		3443		443
+
+cp /sgoinfre/goinfre/Perso/flklein/VM\ roger-skyline.vdi /sgoinfre/goinfre/Perso/flklein/VM\ rs-deploy.vdi &
+ID=$(ps | grep -m1 cp | awk '{print $1}')
+RED='\033[1;31m'
+GRE='\033[1;32m'
+LCY='\033[1;36m'
+YEL='\033[1;33m'
+NC='\033[0m'
+let "i = 0"
+let "n = 1"
+let "s = 0"
+echo "Deployment started on process ${RED}$ID${NC}."
+while [ "$(ps -p $ID | grep $ID)" != "" ]
+do
+	if [ $n -eq 1 ]
+	then
+		printf "${YEL}["
+	elif [ $n -eq 10000 ]
+	then
+		printf "${YEL}] "
+		let "n = 0"
+		if [ $s -eq 0 ]
+		then
+			printf "${GRE}✓ Network\n"
+		fi
+		if [ $s -eq 1 ]
+		then
+			printf "${GRE}✓ Scripts\n"
+		fi
+		if [ $s -eq 2 ]
+		then
+			printf "${GRE}✓ Web\n"
+		fi
+		let "s = s + 1"
+		let "i = 0"
+	elif [ $i -eq 1000 ]
+	then
+		printf "${YEL}❚"
+		let "i = 0"
+	fi
+	let "i = i + 1"
+	let "n = n + 1"
+done
+while [ $n -lt 10000 ]
+do
+	if [ $(($n % 1000)) -eq 0 ]
+	then
+		printf "${YEL}❚"
+	fi
+	let "n = n + 1"
+done
+echo "${YEL}] ${LCY}Done."
