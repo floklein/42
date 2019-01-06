b9c2d46322cdd1f56a90a6613eab0a8b708a11eb
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-07 00:29:38 +0100

    php split

--- PiscinePHP/Day01/ex02/oddeven.php
+++ PiscinePHP/Day01/ex02/oddeven.php
@@ -1,1 +1,17 @@
 <?php
+while (true) {
+    echo "Entrez un nombre : ";
+    if (($input = fgets(STDIN)) == null) {
+        exit("\n");
+    }
+    $input = trim($input);
+    if (is_numeric($input)) {
+        if ($input % 2 == 0) {
+            echo "Le chiffre " . $input . " est Pair\n";
+        } else {
+            echo "Le chiffre " . $input . " est Impair\n";
+        }
+    } else {
+        echo "'" . $input . "' n'est pas un chiffre\n";
+    }
+}
---
+++ PiscinePHP/Day01/ex03/ft_split.php
@@ -0,0 +1,7 @@
+<?php
+function ft_split($str)
+{
+    $arr = explode(" ", trim(preg_replace('/ +/', ' ', $str)));
+    sort($arr);
+    return $arr;
+}
