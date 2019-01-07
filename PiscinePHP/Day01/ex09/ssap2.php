79b22b72fb033903979e00881239727275fa7994
Florent Klein <flo-klein@hotmail.fr>
2019-01-07 02:32:26 +0100

    au lit

--- PiscinePHP/Day01/ex09/ssap2.php
+++ PiscinePHP/Day01/ex09/ssap2.php
@@ -1,1 +1,18 @@
 <?php
+$arr = array();
+for ($i = 1; $i < $argc; $i++) {
+    $arr = array_merge($arr, explode(" ", trim(preg_replace('/ +/', ' ', $argv[$i]))));
+}
+sort($arr, SORT_STRING | SORT_FLAG_CASE);
+foreach ($arr as $value) {
+    if (ctype_alpha($value))
+        echo $value . "\n";
+}
+foreach ($arr as $value) {
+    if (is_numeric($value))
+        echo $value . "\n";
+}
+foreach ($arr as $value) {
+    if (!ctype_alpha($value) && !is_numeric($value))
+        echo $value . "\n";
+}
