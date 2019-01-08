9dd44257efed905e36a066bf11f3a7f66824026a
Florent Klein <flklein@student.42.fr>
2019-01-08 16:34:35 +0100

    on parse du csv

---
+++ PiscinePHP/Day01/ex13/sing_it!.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+if (strcmp($argv[1], "mais pourquoi cette demo ?") == 0) {
+    echo "Tout simplement pour qu'en feuilletant le sujet\non ne s'apercoive pas de la nature de l'exo..\n";
+}
+?>
