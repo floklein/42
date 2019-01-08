f6148b1f91beaa19efb5409036311a0df114925c
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-08 14:55:01 +0100

    search it

--- PiscinePHP/Day01/ex12/search_it!.php
+++ PiscinePHP/Day01/ex12/search_it!.php
@@ -3,4 +3,13 @@
 if ($argc < 3) {
     exit();
 }
+for ($i = 2; $i < $argc; $i++) {
+    $arr = explode(":", $argv[$i]);
+    if (strcmp($arr[0], $argv[1]) == 0) {
+        $value = $arr[1];
+    }
+}
+if ($value != null) {
+    echo $value . "\n";
+}
 ?>
