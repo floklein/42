c95190ff273498da37451fecd081a2e4c10c63c5
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-09 20:18:07 +0100

    matching regexes

--- PiscinePHP/Day02/ex05/deni.php
+++ PiscinePHP/Day02/ex05/deni.php
@@ -1,4 +1,28 @@
 #!/usr/bin/php
 <?php
-
+$csv_file = fopen($argv[1], "r");
+$wanted_key = $argv[2];
+$line = fgets($csv_file);
+$header_keys = explode(";", trim($line));
+if (array_search($wanted_key, $header_keys, true) === false) {
+    exit();
+}
+while ($line = fgets($csv_file)) {
+    $arr = explode(";", $line);
+    $j = 0;
+    foreach ($header_keys as $key) {
+        $tab[$i][$key] = $arr[$j];
+        $j++;
+    }
+    $i++;
+}
+print_r($tab);
+while (true) {
+    echo "Entrez votre commande : ";
+    if (($input = fgets(STDIN)) == null) {
+        exit("\n");
+    }
+    preg_match_all("/\$[^\]]+\[[^\[]+\]/", $input, $matches);
+    eval($input . ";");
+}
 ?>
