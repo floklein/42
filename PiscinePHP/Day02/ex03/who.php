c12d9c735a863bb048cb4aae3722a51bc467cc02
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-09 16:13:39 +0100

    who let the dogs in

--- PiscinePHP/Day02/ex03/who.php
+++ PiscinePHP/Day02/ex03/who.php
@@ -1,4 +1,13 @@
 #!/usr/bin/php
 <?php
-system("who");
+date_default_timezone_set("Europe/Paris");
+$file = fopen("/var/run/utmpx", "r");
+while ($line = fread($file, 628)) {
+    $output[] = unpack("A256user/A4id/A32line/I4time", $line);
+}
+foreach($output as $value) {
+    if ($value[user] !== "utmpx-1.00" && $value[user] != null) {
+        echo $value[user] . "  " . $value[line] . "  " . date("M  j H:i", $value[time3]) . " \n";
+    }
+}
 ?>
