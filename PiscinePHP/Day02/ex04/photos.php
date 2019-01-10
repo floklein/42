ae8ffc51c2fccab333ccad13062fa91fe0ae60cb
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-10 23:20:52 +0100

    PHOTOS

--- PiscinePHP/Day02/ex04/photos.php
+++ PiscinePHP/Day02/ex04/photos.php
@@ -1,23 +1,26 @@
 #!/usr/bin/php
 <?php
 if ($argc < 2) {
-    exit();
+	exit();
 }
 $page = file_get_contents($argv[1]);
 preg_match_all("/<img .*?>/", $page, $tab);
 $stock = $tab[0];
 preg_match("/[^\/]*\$/", $argv[1], $folder_tab);
 $folder = $folder_tab[0] . "/";
+if (file_exists($folder)) {
+	exit ;
+}
 mkdir($folder);
 foreach ($stock as $pic) {
-    preg_match("/src=[\"'].*?[\"']/", $pic, $src_tab);
-    $src = substr($src_tab[0], 5, -1);
-    preg_match("/[^\/]*\$/", $src, $name_tab);
-    $name = $name_tab[0];
-    if (preg_match("/http?.:\/\//", $src)) {
-        copy($src, $folder . $name);
-    } else {
-        copy($argv[1] . "/" . $src, $folder . $name);
-    }
+	preg_match("/src=[\"'].*?[\"']/", $pic, $src_tab);
+	$src = substr($src_tab[0], 5, -1);
+	preg_match("/[^\/]*\$/", $src, $name_tab);
+	$name = $name_tab[0];
+	if (preg_match("/http?.:\/\//", $src)) {
+		copy($src, $folder . $name);
+	} else {
+		copy($argv[1] . "/" . trim($src, "/"), $folder . $name);
+	}
 }
 ?>
