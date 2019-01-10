b4016092604971ba698538892b548846a10d3f81
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-10 22:56:27 +0100

    WHO

--- PiscinePHP/Day02/ex03/who.php
+++ PiscinePHP/Day02/ex03/who.php
@@ -3,11 +3,12 @@
 date_default_timezone_set("Europe/Paris");
 $file = fopen("/var/run/utmpx", "r");
 while ($line = fread($file, 628)) {
-    $output[] = unpack("A256user/A4id/A32line/I4time", $line);
+    $output[] = unpack("A256user/A4id/A32tty/Ipid/Itype/Ltime/Lother/A256host/A64pad", $line);
 }
+sort($output);
 foreach($output as $value) {
-    if ($value[user] !== "utmpx-1.00" && $value[user] != null) {
-        echo $value[user] . "  " . $value[line] . "  " . date("M  j H:i", $value[time3]) . " \n";
+    if ($value['user'] !== "utmpx-1.00" && $value['user'] != null && $value['type'] == 7) {
+		echo $value['user'] . "  " . $value['tty'] . "  " . date("M j H:i", $value['time']) . " \n";
     }
 }
 ?>
