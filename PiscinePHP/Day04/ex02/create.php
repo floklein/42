72b20a31348be7bbddc4ff0990541bc162bfd887
Florent Klein <flklein@student.42.fr>
2019-01-11 00:47:23 +0100

    le push de la nuit

--- PiscinePHP/Day04/ex02/create.php
+++ PiscinePHP/Day04/ex02/create.php
@@ -6,6 +6,6 @@ echo $passwd . "\n";
 if (!$login || !$passwd) {
     exit("ERROR\n");
 }
-$pwd_file = file_get_contents("/private/passwd");
+$pwd_file = file_get_contents("../private/passwd");
 $tab = unserialize($pwd_file);
 print_r($tab);
