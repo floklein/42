aab35f5da6ef8bc48f2f6887226f53f29626c8d7
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-10 20:07:11 +0100

    session loginpwd

--- PiscinePHP/Day04/ex01/index.php
+++ PiscinePHP/Day04/ex01/index.php
@@ -1,14 +1,20 @@
 <?php
+session_start();
+if ($_GET['login'] != null) {
+    $_SESSION['login'] = $_GET['login'];
+}
+if ($_GET['passwd'] != null) {
+    $_SESSION['passwd'] = $_GET['passwd'];
+}
 ?>
 
+<!DOCTYPE html>
 <html>
 <body>
-
 <form action="index.php" method="GET">
-<input type="text" name="login" placeholder="login"><br>
-<input type="password" name="passwd" placeholder="passwd"><br>
-<button type="submit">OK</button>
+Identifiant: <input type="text" name="login" placeholder="login" value=<?php echo $_SESSION['login']; ?>><br>
+Mot de passe: <input type="password" name="passwd" placeholder="passwd" value=<?php echo $_SESSION['passwd']; ?>><br>
+<button type="submit" name="submit" value="OK">OK</button>
 </form>
-
 </body>
 </html>
