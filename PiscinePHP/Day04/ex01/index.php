7ec27078d838bd3b8b88830b14fd29201be1b962
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-10 18:51:32 +0100

    session dasso

---
+++ PiscinePHP/Day04/ex01/index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<html>
+<body>
+
+<form action="index.php" method="GET">
+<input type="text" name="login" placeholder="login"><br>
+<input type="password" name="passwd" placeholder="passwd"><br>
+<button type="submit">OK</button>
+</form>
+
+</body>
+</html>
