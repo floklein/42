a34b055b05587a3ac78eb286440e2ab4e8540a69
Florent Klein <flo-klein@hotmail.fr>
2019-01-11 05:33:10 +0100

    debut rush00

---
+++ PiscinePHP/Rush00/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
+</head>
+
+<body>
+    <div class="top-div">
+        <p class="top-txt">- LEAFY -</p>
+    </div>
+    <ul style="top: 150px">
+        <li><a class="active" href="index.php">Accueil</a></li>
+        <li class="dropdown">
+            <a href="#sandwiches" class="dropbtn">Homme <img src="img/arrow.png" class="img-arrow"></a>
+            <div class="dropdown-content">
+                <a href="#classique">Hauts</a>
+                <a href="#grand">Pantalons</a>
+                <a href="#veggie">Accessoires</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#boissons" class="dropbtn">Femme <img src="img/arrow.png" class="img-arrow"></a>
+            <div class="dropdown-content">
+                <a href="#sodas">Robes</a>
+                <a href="#chaudes">Hauts</a>
+                <a href="#chaudes">Pantalons</a>
+                <a href="#alcools">Sacs</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#desserts" class="dropbtn">Enfants <img src="img/arrow.png" class="img-arrow"></a>
+            <div class="dropdown-content">
+                <a href="#tartes">T-shirts</a>
+                <a href="#glaces">Jeans</a>
+            </div>
+        </li>
+        <li class="dropdown" style="float:right">
+            <a href="#about" class="dropbtn"><img src="img/bag.png" class="img-bag"></a>
+        </li>
+        <li class="dropdown" style="float:right">
+            <a href="#about" class="dropbtn">Mon compte <img src="img/arrow.png" class="img-arrow"></a>
+            <div class="dropdown-content">
+                <a href="#about">Mes commandes</a>
+                <a href="#about">Mes preferences</a>
+                <a href="#about">Se deconnecter</a>
+            </div>
+        </li>
+    </ul>
+</body>
+
+</html>
