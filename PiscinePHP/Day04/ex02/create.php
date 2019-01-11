418b837e1c62ac8ddcfeeca9c217155f8d9cd822
Florent Klein <flklein@student.42.fr>
2019-01-11 14:37:19 +0100

    La Création

--- PiscinePHP/Day04/ex02/create.php
+++ PiscinePHP/Day04/ex02/create.php
@@ -1,11 +1,29 @@
 <?php
 $login = $_POST['login'];
 $passwd = $_POST['passwd'];
-echo $login . "\n";
-echo $passwd . "\n";
 if (!$login || !$passwd) {
     exit("ERROR\n");
 }
-$pwd_file = file_get_contents("../private/passwd");
-$tab = unserialize($pwd_file);
+if (file_exists("../private/passwd")) {
+    $pwd_file = file_get_contents("../private/passwd");
+    $tab = unserialize($pwd_file);
+} else {
+    $tab = array();
+}
 print_r($tab);
+foreach ($tab as $key=>$user) {
+    if ($user['login'] == $login) {
+        exit("ERROR\n");
+    }
+}
+$tab[$key + 1]['login'] = $login;
+$tab[$key + 1]['passwd'] = hash("sha256",$passwd);
+$out = serialize($tab);
+file_put_contents("../private/passwd", $out);
+if (file_exists("../private/passwd")) {
+    $pwd_file = file_get_contents("../private/passwd");
+    $tab = unserialize($pwd_file);
+}
+print_r($tab);
+echo "OK\n"
+?>
