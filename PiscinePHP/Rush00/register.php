e15eff8e9e7567303ff5626dee6e8488deada4b1
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-13 00:52:16 +0100

    fin samedi

--- PiscinePHP/Rush00/register.php
+++ PiscinePHP/Rush00/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>S'inscrire</title>
+    <title>LEAFY - S'inscrire</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/signin.css" />
     <link rel="icon" type="image/x-icon" href="favicon.ico?v=1" />
