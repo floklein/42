0901be2b2e6117ddc14f0bf07775003c47a86583
Florent Klein <flklein@student.42.fr>
2019-01-12 19:10:25 +0100

    mario cart

--- PiscinePHP/Rush00/add_to_cart.php
+++ PiscinePHP/Rush00/add_to_cart.php
@@ -6,7 +6,6 @@ $subcat = $_GET['subcat'];
 $price = $_GET['price'];
 $img = $_GET['img'];
 $size = $_POST['size'];
-var_dump($size);
 if (!isset($_COOKIE['cart'])) {
     $tab = array();
 } else {
@@ -26,10 +25,6 @@ switch ($action) {
         break;
     case "del":
         foreach($tab as $key=>$elem) {
-            var_dump($elem['name'] == $name);
-            var_dump($elem['cat'] == $cat);
-            var_dump($elem['subcat'] == $subcat);
-            var_dump($elem['size'] == $size);
             if ($elem['name'] == $name && $elem['cat'] == $cat && $elem['subcat'] == $subcat && $elem['size'] == $size) {
                 unset($tab[$key]);
                 setcookie("cart", serialize($tab), time() + 86400);
