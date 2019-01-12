70510a484e02f15550123a0f277a2d4861cef71d
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-12 15:49:47 +0100

    gestion utilisateur

---
+++ PiscinePHP/Rush00/login.php
@@ -0,0 +1,27 @@
+<?php
+include "auth.php";
+session_start();
+$login = $_POST['login'];
+$passwd = $_POST['passwd'];
+if (auth($login, $passwd)) {
+    $_SESSION['logged_on_user']['login'] = $login;
+    $_SESSION['logged_on_user']['passwd'] = $passwd;
+    header("Location: index.php");
+} else {
+    $_SESSION['logged_on_user'] = "";
+    exit("ERROR\n");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Connecté</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <a href="index.php">Retour</a>
+</body>
+</html>
