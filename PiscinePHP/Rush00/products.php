2cccc7ffc289a88cf25e9e697f8b070e39b9f5d6
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-13 19:39:17 +0100

    search

--- PiscinePHP/Rush00/products.php
+++ PiscinePHP/Rush00/products.php
@@ -53,6 +53,12 @@ $categories = unserialize($categories_file);
                 ?>
             </div>
         </li>
+        <li class="dropdown" style="float:right">
+		    <form method="GET" action="search.php" class="dropbtn">
+			    <input class="search-bar" type="text" name="search" placeholder="Rechercher...">
+                <button class="search-button" type="submit"><img class="img-loupe" src="img/loupe.png"></button>
+		    </form>
+	    </li>
     </ul>
     <div class="main">
         <div class="title">
