6a307c46f863f01cd58cd732a8d21b7b59d898c5
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-13 03:11:23 +0100

    logged user carts

--- PiscinePHP/Rush00/login.php
+++ PiscinePHP/Rush00/login.php
@@ -3,9 +3,16 @@ include "auth.php";
 session_start();
 $login = $_POST['login'];
 $passwd = $_POST['passwd'];
+$carts_file = file_get_contents("private/carts");
+$carts = unserialize($carts_file);
 if (auth($login, $passwd)) {
     $_SESSION['logged_on_user']['login'] = $login;
     $_SESSION['logged_on_user']['passwd'] = $passwd;
+    if ($carts[$login] == null) {
+        $carts[$login] = unserialize($_COOKIE['cart']);
+        $out = serialize($carts);
+        file_put_contents("private/carts", $out);
+    }
     header("Location: index.php");
 } else {
     $_SESSION['logged_on_user'] = "";
