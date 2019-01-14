619dc1888572d147ac1f671445ef1e37a8758ce1
Florent Klein <flklein@student.42.fr>
2019-01-14 20:56:44 +0100

    sql ex07

---
+++ PiscinePHP/Day05/ex03/ex03.sql
@@ -0,0 +1,1 @@
+INSERT INTO ft_table (login, `group`, creation_date) SELECT last_name, 'other', birthdate FROM user_card WHERE last_name LIKE '%a%' AND LENGTH(last_name) < 9 ORDER BY `last_name` ASC LIMIT 10;
