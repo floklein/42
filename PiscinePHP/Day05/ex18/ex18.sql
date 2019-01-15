c5cd43766e310ea23f8a1585d79cf0d6044a6b83
Florent Klein <flklein@student.42.fr>
2019-01-15 19:32:30 +0100

    trop class

--- PiscinePHP/Day05/ex18/ex18.sql
+++ PiscinePHP/Day05/ex18/ex18.sql
@@ -1,6 +1,7 @@
 SELECT name FROM distrib
     WHERE id_distrib = 42
-        OR id_distrib BETWEEN 62 AND 71
+        OR id_distrib BETWEEN 62 AND 69
+        OR id_distrib = 71
         OR id_distrib BETWEEN 88 AND 90
         OR LENGTH(name) - LENGTH(REPLACE(name, 'y', '')) = 2
         OR LENGTH(name) - LENGTH(REPLACE(name, 'Y', '')) = 2
