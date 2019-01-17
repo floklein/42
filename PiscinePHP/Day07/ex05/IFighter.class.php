7873aed302c5394f758e34366eaaabd33091cf5f
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-17 19:22:22 +0100

    la castagne

---
+++ PiscinePHP/Day07/ex05/IFighter.class.php
@@ -0,0 +1,6 @@
+<?php
+interface IFighter
+{
+    public function fight();
+}
+?>
--- PiscinePHP/Day07/ex05/NightsWatch.class.php
+++ PiscinePHP/Day07/ex05/NightsWatch.class.php
@@ -1,3 +1,18 @@
 <?php
-
+class NightsWatch implements IFighter
+{
+    private $_team = array();
+    public function recruit($character)
+    {
+        $this->_team[] = $character;
+    }
+    public function fight()
+    {
+        foreach ($this->_team as $char) {
+            if (method_exists($char, 'fight')) {
+                $char->fight();
+            }
+        }
+    }
+}
 ?>
