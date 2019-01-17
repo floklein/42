cdebc332c662bd52f7a556da72b1e4fed6bfe472
Florent Klein <flklein@student.42.fr>
2019-01-17 21:04:30 +0100

    Merci Yaya

--- PiscinePHP/Day07/ex06/Fighter.class.php
+++ PiscinePHP/Day07/ex06/Fighter.class.php
@@ -1,2 +1,15 @@
 <?php
+abstract class Fighter
+{
+    abstract public function fight($target);
+
+    public function __construct($soldier)
+    {
+        $this->soldier_type = $soldier;
+    }
+    public function getType()
+    {
+        return ($this->soldier_type);
+    }
+}
 ?>
