17a11136aa0855797ca5fc9dd5904c5eeba3e96e
Florent Klein <flklein@student.42.fr>
2019-01-21 20:03:20 +0100

    instalike

--- Web/Camagru/index.php
+++ Web/Camagru/index.php
@@ -4,33 +4,9 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Instacam</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="css/index.css">
-</head>
-
-<body>
-
-  <div id="navbar">
-    <a href="#feed" id="logo" class="left-buttons">Instacam</a>
-    <div id="navbar-right">
-      <a href="#camera"><img src="/img/camera.png"></a>
-      <a href="#feed"><img src="/img/feed.png"></a>
-      <a href="#account"><img src="/img/account.png"></a>
-    </div>
-  </div>
-
-  <div style="margin-top:210px;padding:15px 15px 2500px;font-size:30px">
-    <p><b>This example demonstrates how to shrink a navigation bar when the user starts to scroll the page.</b></p>
-    <p>Scroll down this frame to see the effect!</p>
-    <p>Scroll to the top to remove the effect.</p>
-    <p><b>Note:</b> We have also made the navbar responsive, resize the browser window to see the effect.</p>
-    <p>Lorem ipsum dolor dummy text to enable scrolling, sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-      deserunt mollit anim id est laborum.</p>
-  </div>
-
   <script>
     window.onscroll = function () { scrollFunction() };
 
@@ -44,7 +20,37 @@
       }
     }
   </script>
+</head>
 
+<body>
+
+  <div id="navbar">
+    <a href="#feed" id="logo" class="left-buttons">Instacam</a>
+    <div id="navbar-right">
+      <a href="#camera"><img src="/img/camera.png"></a>
+      <a href="#feed"><img src="/img/feed.png"></a>
+      <a href="#account"><img src="/img/account.png"></a>
+    </div>
+  </div>
+  <div id="feed">
+    <div id="feed-box">
+      <div id="feed-header">
+        <div id="feed-user-pic" style="background-image: url('/resources/profile-pics/0.jpg');">
+        </div>
+        <div id="feed-user-login">
+          <a href="#user"><p>florentklein</p></a>
+        </div>
+      </div>
+      <div id="feed-pic" style="background-image: url('/resources/feed-pics/0.jpg');">
+      </div>
+    </div>
+    <div id="feed-box">
+    </div>
+    <div id="feed-box">
+    </div>
+    <div id="feed-box">
+    </div>
+  </div>
 </body>
 
 </html>
