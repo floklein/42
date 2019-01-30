a510510b3c99ace83f56213f168367351eef074e
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-01-30 16:37:44 +0100

    on commence la pdo

--- Web/Camagru/reset-pwd.php
+++ Web/Camagru/reset-pwd.php
@@ -13,19 +13,19 @@
 <div id="navbar">
     <a href="/index.php" id="logo" class="left-buttons">Instacam</a>
     <div id="navbar-right">
-        <a href="/camera.php"><img src="/assets/camera.png"></a>
-        <a href="/index.php"><img src="/assets/feed.png"></a>
-        <a href="/account.php"><img src="/assets/account.png"></a>
+        <a href="/camera.php"><img src="/assets/camera.png" alt="camera"></a>
+        <a href="/index.php"><img src="/assets/feed.png" alt="feed"></a>
+        <a href="/account.php"><img src="/assets/account.png" alt="account"></a>
     </div>
 </div>
 
 <div id="signin-box">
     <p>Choisissez un <span>nouveau</span> mot de passe.</p>
     <form>
-        <input type="password" placeholder="Votre nouveau mot de passe"><br>
-        <input type="password" placeholder="Confirmez le mot de passe"><br>
+        <input type="password" name="pwd" placeholder="Votre nouveau mot de passe"><br>
+        <input type="password" name="pwd-confirm" placeholder="Confirmez le mot de passe"><br>
         <div id="signin-button">
-            <button class="signin-button" type="submit">Modifier</button>
+            <button type="submit" name="resetpwd-submit" value="ok">Modifier</button>
         </div>
     </form>
 </div>
