ac2eb9b4f88dde0b0f5276ffed2b6a497f777251
Florent Klein <flo-klein@hotmail.fr>
2019-02-05 15:00:16 +0100

    PDO::ERRMODE_EXCEPTION actif

--- Web/Camagru/back/login.php
+++ Web/Camagru/back/login.php
@@ -12,7 +12,7 @@ if (auth($login, $passwd)) {
     try {
         $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        $pdo->setAttribute(PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOEXCEPTION $e) {
         exit($e);
     }
--- Web/Camagru/back/upload_pp.php
+++ Web/Camagru/back/upload_pp.php
@@ -46,7 +46,7 @@ $DB_DSN .= ";dbname=" . $DB_NAME;
 try {
     $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $pdo->setAttribute(PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOEXCEPTION $e) {
     exit($e);
 }
