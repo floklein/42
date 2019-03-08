d4e45525c70ddfb607c0765abefc8a65fa1ab529
Florent Klein <flo-klein@hotmail.fr>
2019-03-08 14:05:32 +0100

    todolist React done

---
+++ Random/React/todolist/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. It is my first React app.</p>
+    </React.Fragment>
+  )
+}
