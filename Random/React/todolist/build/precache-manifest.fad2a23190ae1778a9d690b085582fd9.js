97077fc81463ff7269e655ecb5f37ce7bfb82a46
Florent Klein <flo-klein@hotmail.fr>
2019-03-08 14:06:23 +0100

    build

---
+++ Random/React/todolist/build/precache-manifest.fad2a23190ae1778a9d690b085582fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac4d5b3e4d5d0db6779c",
+    "url": "/static/css/main.3c2911c5.chunk.css"
+  },
+  {
+    "revision": "ac4d5b3e4d5d0db6779c",
+    "url": "/static/js/main.d87d44e6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ecd69f3c6bd9db64d160",
+    "url": "/static/js/2.18d55f00.chunk.js"
+  },
+  {
+    "revision": "62fc723fa060a78544c2441d57942f65",
+    "url": "/index.html"
+  }
+];
