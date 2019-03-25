5a893b9214f75611adfc4186eed1d410b85fb4e8
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-03-25 18:46:17 +0100

    mern

--- Random/MERN/starterpack/client/src/components/customers/customers.js
+++ Random/MERN/starterpack/client/src/components/customers/customers.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './customers.css';
 
 class Customers extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Customers</h2>
-            </div>
-        );
+  constructor() {
+    super();
+    this.state = {
+      customers: []
     }
+  }
+
+  componentDidMount() {
+    fetch('/api/customers')
+      .then(res => res.json())
+      .then(customers =>
+        this.setState({customers: customers}, () =>
+          console.log('Customers fetched...', customers)
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Customers</h2>
+        <ul>
+          {this.state.customers.map(customer =>
+            <li key={customer.id}>{customer.firstName} {customer.lastName}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Customers;
