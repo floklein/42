61670b52d836d34237839fcd535c38f2b68dbafa
Florent Klein <4128942+floklein@users.noreply.github.com>
2019-04-06 12:29:12 +0200

    map

---
+++ Random/Matcha/FullStackDev/client/src/components/Footer/footer.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import './footer.css'
+import triCouple from "../../assets/img/tri-couple.svg";
+import triMap from "../../assets/img/tri-map.svg";
+import triChat from "../../assets/img/tri-chat.svg";
+
+class Footer extends Component {
+
+  render() {
+
+    return (
+      <div id="footer" className="centered">
+        <div>
+          <div>
+            <h4>Soulmatch</h4>
+            <a href="#"><p>Accueil</p></a>
+            <a href="#"><p>Soulmatcher</p></a>
+            <a href="#"><p>Recherche</p></a>
+          </div>
+          <div>
+            <h4>Mon compte</h4>
+            <a href="#"><p>Mon profil</p></a>
+            <a href="#"><p>Mes préférences</p></a>
+            <a href="#"><p>Déconnexion</p></a>
+          </div>
+          <div>
+            <h4>L'équipe</h4>
+            <a href="#"><p>Florent Klein</p></a>
+            <a href="#"><p>Tanguy Boissel</p></a>
+          </div>
+          <div>
+            <h4>À propos</h4>
+            <a href="#"><p>© 2019 Soulmatch</p></a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
