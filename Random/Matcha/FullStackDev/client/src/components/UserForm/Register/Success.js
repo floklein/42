bef07739811770e455209513371458e7a5ebfc22
Florent Klein <flklein@student.42.fr>
2019-04-06 19:56:47 +0200

    success svg

---
+++ Random/Matcha/FullStackDev/client/src/components/UserForm/Register/Success.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import imgSuccess from '../../../assets/img/success.svg'
+
+export class Success extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <img src={imgSuccess} alt="success"/>
+        <h2>Vous êtes inscrit !</h2>
+      </React.Fragment>);
+  }
+}
+
+export default Success;
