a0796d7d1d48f739587b85a4bc1d5b48dd133ac2
Florent Klein <flklein@student.42.fr>
2019-04-07 21:58:28 +0200

    animations

--- Random/Matcha/FullStackDev/client/src/components/UserForm/Login/LoginForm.js
+++ Random/Matcha/FullStackDev/client/src/components/UserForm/Login/LoginForm.js
@@ -1,19 +1,28 @@
 import React, {Component} from 'react';
+import {Spring, config} from 'react-spring/renderprops';
 
 export class LoginForm extends Component {
   render() {
     return (
       <React.Fragment>
-        <h2>Se connecter.</h2>
-        <p className="subtitle">Pas de compte ? <span onClick={this.props.gotoRegister}>Inscrivez-vous !</span></p>
-        <input type="text" name="login" placeholder="Votre pseudo ou votre email"
-               title="Votre pseudo ou votre email" required minLength="1" maxLength="64"/>
-        <p>&nbsp;</p>
-        <input type="password" name="password" placeholder="Votre mot de passe"
-               title="Votre mot de passe" required
-               minLength="8" maxLength="64"/>
-        <p>&nbsp;</p>
-        <button className="green">Connexion</button>
+        <Spring
+          config={config.molasses}
+          from={{opacity: 0}}
+          to={{opacity: 1}}
+          leave={{opacity: 0}}>
+          {props => <div style={props}>
+            <h2>Se connecter.</h2>
+            <p className="subtitle">Pas de compte ? <span onClick={this.props.gotoRegister}>Inscrivez-vous !</span></p>
+            <input type="text" name="login" placeholder="Votre pseudo ou votre email"
+                   title="Votre pseudo ou votre email" required minLength="1" maxLength="64"/>
+            <p>&nbsp;</p>
+            <input type="password" name="password" placeholder="Votre mot de passe"
+                   title="Votre mot de passe" required
+                   minLength="8" maxLength="64"/>
+            <p>&nbsp;</p>
+            <button className="green">Connexion</button>
+          </div>}
+        </Spring>
       </React.Fragment>);
   }
 }
